70878cc40d7fb301384a1fb68fc3a442778186cd
shinto.cv <cshintov@gmail.com>
2020-03-18 19:09:47 +0530

    Add sample code for chapter 'functional programming'

---
+++ chapter5-fp/ch5/src/ch5/core.clj
@@ -0,0 +1,45 @@
+(ns ch5.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+
+(defn two-comp
+  [f g]
+  (fn [& args ] 
+    (f (apply g args))))
+
+((two-comp inc *) 2 3)
+
+(defn mycomp
+  [& functions]
+  (do
+    (println functions)
+    (fn [& args] 
+      (apply (reduce two-comp
+                     identity
+                     functions) args))))
+;;  (apply comp functions args))
+
+((mycomp #(+ % 2) inc * ) 2 3)
+
+(defn sleepy-id [x]
+  (Thread/sleep 1000)
+  x)
+
+(time (sleepy-id "Me"))
+
+(def memoized-sleepy-id (memoize sleepy-id))
+
+(time (memoized-sleepy-id "Me"))
+(time (memoized-sleepy-id "Me"))
+
+(defn fib [n]
+  (if (< n 2)
+    n
+    (+ (fib (- n 1)) (fib (- n 2)))))
+
+(def fib (memoize fib))
