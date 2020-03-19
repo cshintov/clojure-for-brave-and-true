68196381e0ff118a046a0502a23098e35220f417
shinto.cv <cshintov@gmail.com>
2020-03-19 20:26:22 +0530

    Add gitignore

--- peg-thing/src/peg_thing/core.clj
+++ peg-thing/src/peg_thing/core.clj
@@ -1,6 +1,25 @@
 (ns peg-thing.core
   (:gen-class))
 
+(declare successful-move prompt-move game-over query-rows)
+
+
+(defn tri*
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [newsum (+ sum n)]
+     (cons newsum (lazy-seq (tri* newsum (inc n)))))))
+
+(def tri (tri*))
+
+(take 5 tri)
+
+(defn triangular?
+  [n]
+  (= n (last (take-while #(<= n) tri))))
+
+(triangular? 1)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
