2f2c3a49d83adbe5aeb6a469fdbe33b0f5f33e16
shinto.cv <cshintov@gmail.com>
2020-03-23 06:46:32 +0530

    SICP lecture 2b

---
+++ chapter-7-clojure-alchemy/ch7/src/ch7/core.clj
@@ -0,0 +1,22 @@
+(ns ch7.core
+  (:gen-class))
+
+
+;; implementing pair abstraction in terms of procedures
+;; procedure as data
+;; cons returns a fn instead of a data structure
+(defn cons' [x y]
+  (fn [pick]
+    (if (= pick 1) x
+        (if  (= pick 2) y))))
+
+(defn car' [x] (x 1))
+
+(defn cdr' [x] (x 2))
+
+;; pair abstraction
+(def x (cons' 2 3))
+(= (car' x) 2)
+(= (cdr' x) 3)
+
+
